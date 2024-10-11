e5670267150789996529384dca2e7aa6331084a6
Ken <134857008+kenStinks@users.noreply.github.com>
2024-10-11 14:15:06 +0800

    processes are now multithreaded.

--- ScreenFactory.h
+++ ScreenFactory.h
@@ -6,6 +6,12 @@
 #include <time.h> 
 using namespace std;
 
+enum Status {
+	READY,
+	WAITING,
+	RUNNING,
+	TERMINATED
+};
 
 class ScreenFactory
 {
@@ -14,12 +20,14 @@ class ScreenFactory
 		string timeCreated;
 		int lineOfInstruction;
 		int totalLineofInstruction;
+		Status status;
 	
 	public:
-		ScreenFactory(string name) {
+		ScreenFactory(string name, int total_instruction) {
 			this->name = name;
 			this->lineOfInstruction = 0;
-			this->totalLineofInstruction = 0;
+			this->totalLineofInstruction = total_instruction;
+			this->status = READY;
 			initializeTimeCreated();
 		}
 
@@ -31,12 +39,39 @@ class ScreenFactory
 			return name;
 		}
 
+		Status getStatus() const {
+			return status;
+		}
+
+		void setStatus(Status s) {
+			status = s;
+		}
+
 		int getLineOfInstruction() {
 			return lineOfInstruction;
 		}
 
 		int getTotalLineofInstruction() {
 			return totalLineofInstruction;
+		}
+
+		void print(int core) {
+			if (status == RUNNING) {
+				if (lineOfInstruction < totalLineofInstruction) {
+					//print thing
+					time_t now = time(0);
+					tm localTime;
+					localtime_s(&localTime, &now);
+
+					char output[50];
+
+					strftime(output, 50, "(%m/%d/%y %H:%M:%S %p)", &localTime);
+
+					//cout << output << " Core:" << core << " \"Hello world from " << name << "!\"\n";
+					lineOfInstruction+=1;
+				}
+				else status = TERMINATED;
+			}
 		}
 
 
