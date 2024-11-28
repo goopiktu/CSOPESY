46263a6bb6f7084aeca3a999447a3ea7c2aa12fd
Aldwin <91401877+goopiktu@users.noreply.github.com>
2024-11-29 04:31:19 +0800

    Paging

--- Memory.h
+++ Memory.h
@@ -90,3 +90,72 @@ public:
 		return std::string(memory.begin(), memory.end());
 	}
 };
+
+
+class PagingAllocator : public IMemoryAllocator {
+private:
+	size_t maxMemorySize;
+	size_t frameSize;
+	size_t numFrames;
+	std::vector<size_t> memory;                         // Memory as frames
+	std::unordered_map<size_t, std::vector<size_t>> processFrameMap; // Tracks frames per process
+	std::vector<size_t> freeFrameList;                  // List of free frames
+
+	size_t allocateFrames(size_t numFrames, size_t processId) {
+		if (numFrames > freeFrameList.size()) {
+			throw std::runtime_error("Not enough free frames.");
+		}
+
+		std::vector<size_t> allocatedFrames;
+		for (size_t i = 0; i < numFrames; ++i) {
+			size_t frameIndex = freeFrameList.back();
+			freeFrameList.pop_back();
+			allocatedFrames.push_back(frameIndex);
+			memory[frameIndex] = processId; // Mark frame as occupied
+		}
+
+		processFrameMap[processId] = allocatedFrames;
+		return allocatedFrames[0];
+	}
+
+	void deallocateFrames(size_t processId) {
+		if (processFrameMap.find(processId) == processFrameMap.end()) {
+			throw std::runtime_error("Process ID not found.");
+		}
+
+		for (size_t frameIndex : processFrameMap[processId]) {
+			memory[frameIndex] = 0;
+			freeFrameList.push_back(frameIndex);
+		}
+
+		processFrameMap.erase(processId);
+	}
+
+public:
+	PagingAllocator(size_t maxMemorySize, size_t frameSize)
+		: maxMemorySize(maxMemorySize), frameSize(frameSize),
+		numFrames(maxMemorySize / frameSize), memory(numFrames, 0) {
+		for (size_t i = 0; i < numFrames; ++i) {
+			freeFrameList.push_back(i);
+		}
+	}
+
+	void* allocate(size_t size) override {
+		size_t numFramesNeeded = (size + frameSize - 1) / frameSize;
+		size_t processId = size; 
+		return reinterpret_cast<void*>(allocateFrames(numFramesNeeded, processId));
+	}
+
+	void deallocate(void* ptr, size_t size) override {
+		size_t processId = size; 
+		deallocateFrames(processId);
+	}
+
+	size_t getMaximumSize() override {
+		return maxMemorySize;
+	}
+
+	size_t getAllocatedSize() override {
+		return (numFrames - freeFrameList.size()) * frameSize;
+	}
+};
